f9ba718346c645ac05a7b7167cbb0221c40442bb
sansaha <sansaha@lexmark.com>
2014-10-22 15:15:00 +0530

    incomplete change of print message

--- ExploreGit/src/com/lexmark/misc/MainTest.java
+++ ExploreGit/src/com/lexmark/misc/MainTest.java
@@ -5,7 +5,6 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT learning..!!");
 		System.out.println("Congratulation...You have almost completed the first part of the training...");
-		System.out.println("----------------------------------------------------------------------------");
 		//System.out.println("You have started the second part of the training");
 	}
 
