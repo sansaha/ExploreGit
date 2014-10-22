b4366264268d2555bbace338b05615e01ec55519
sansaha <sansaha@lexmark.com>
2014-10-22 14:54:16 +0530

    fixed print statement for completing lesson 1

--- ExploreGit/src/com/lexmark/misc/MainTest.java
+++ ExploreGit/src/com/lexmark/misc/MainTest.java
@@ -4,7 +4,7 @@ public class MainTest {
 	
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT learning..!!");
-		System.out.println("Congratulation...You have almost completed the first part of the training...");
+		System.out.println("Congratulation...You have completed the first part of the training...");
 	}
 
 }
