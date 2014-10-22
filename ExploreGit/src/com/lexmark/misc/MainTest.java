110112ca0d1c579670d6e2d12e3099ac10d93c7d
sansaha <sansaha@lexmark.com>
2014-10-22 15:15:40 +0530

    fixed print message

--- ExploreGit/src/com/lexmark/misc/MainTest.java
+++ ExploreGit/src/com/lexmark/misc/MainTest.java
@@ -5,6 +5,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT learning..!!");
 		System.out.println("Congratulation...You have completed the first part of the training...");
+		System.out.println("----------------------------------------------------------------------------");
 	}
 
 }
