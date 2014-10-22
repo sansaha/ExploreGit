882db628f1695d268a89ba4f9c536f0228ee9376
sansaha <sansaha@lexmark.com>
2014-10-22 15:25:58 +0530

    print message added for lesson 2

--- ExploreGit/src/com/lexmark/misc/MainTest.java
+++ ExploreGit/src/com/lexmark/misc/MainTest.java
@@ -5,7 +5,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		System.out.println("Welcome to GIT learning..!!");
 		System.out.println("Congratulation...You have almost completed the first part of the training...");
-		//System.out.println("You have started the second part of the training");
+		System.out.println("You have started the second part of the training");
 	}
 
 }
